2c83c100239f682a44be32ef11e31ebce4039468
Maxxxxxxxxxxx <maxxng1028@gmail.com>
2023-04-06 10:30:46 +0800

    File sync

--- Assets/Scripts/Managers/ElementManager.cs
+++ Assets/Scripts/Managers/ElementManager.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public class ElementManager : MonoBehaviour{
+    private string Element;
+     
+
+    public void InitElement(){
+        if (this.Element != null){
+            return;
+        }
+        string[] AllowedElements = {"metal", "wood", "aqua", "fire", "earth"};
+        this.Element = AllowedElements[Random.Range(0, AllowedElements.Length)];
+
+}
+
 /*
     金 = Metal
     木 = Wood
@@ -11,14 +24,3 @@ using UnityEngine;
     土 = Earth
 */
 
-public class ElementManager{
-    private string Element;
-    public void InitElement(){
-        if (this.Element != null){
-            return;
-        }
-        string[] AllowedElements = {"metal", "wood", "aqua", "fire", "earth"};
-        this.Element = AllowedElements[Random.Range(0, AllowedElements.Length)];
-    }
-    
-}
